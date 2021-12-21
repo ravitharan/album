4b97d6e3fa7d221225ab673b0b3ddcea9b2fc5e6
Jeevanantham Anitha <aniththaaniththa149@gmail.com>
2021-12-22 00:37:06 +0530

    the datetime added to image

---
+++ datetime_place_add_to_image.py
@@ -0,0 +1,42 @@
+from get_metadata import *
+import sys
+import os
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+def date_time_location_add_to_image(image, metadata):
+    """
+        The date time and place will be added in the lower left corner of the image
+            '2020-09-26 10:01:46 , .......'
+        
+    """
+    image_name = (os.path.split(image)[-1])
+    image = Image.open(image_name)
+    width, height = image.size
+    
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("FONTS/arial.ttf",40)
+    
+    new_width = (width-(width-40))
+    new_height = (height-80)
+    Date_Time = str(metadata['DateTime']) # 2020-09-26 10:01:46
+
+    if width > height:
+        draw.text((new_width,new_height),Date_Time,(0,0,0),font=font)
+    else:
+        draw.text((new_width,new_height),Date_Time,(0,0,0),font=font)    
+        
+    data_add_image = image.save("COPY--"+image_name)
+
+    return data_add_image
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print(f'Argument error\nUsage: {sys.argv[0]} <jpeg_file>\n')
+        exit(1)
+    image = sys.argv[1]
+    metadata = get_picture_metadata(image)
+    data_add_image = date_time_location_add_to_image(image, metadata)
+    
+
