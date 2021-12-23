66db20cf3550606299873bdc9e2df3891b1d4331
Jeevanantham Anitha <aniththaaniththa149@gmail.com>
2021-12-23 20:55:37 +0530

    location add to image

--- datetime_place_add_to_image.py
+++ datetime_place_add_to_image.py
@@ -1,14 +1,15 @@
 from get_metadata import *
+from get_shooting_place import *
 import sys
 import os
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-def date_time_location_add_to_image(image, metadata):
+def date_time_location_add_to_image(image, metadata,location):
     """
         The date time and place will be added in the lower left corner of the image
-            '2020-09-26 10:01:46 , .......'
+            '2020-09-26 10:01:46 , Kadaiparichchan'
         
     """
     image_name = (os.path.split(image)[-1])
@@ -17,17 +18,17 @@ def date_time_location_add_to_image(image, metadata):
     
     draw = ImageDraw.Draw(image)
     Date_Time = str(metadata['DateTime']) # 2020-09-26 10:01:46
-
+    location = str(location) # Kadaiparichchan, තිරිකුණාමළය දිස්ත්‍රික්කය, கிழக்கு மாகாணம், ශ්‍රී ලංකාව இலங்கை
     if width > height:
         new_width = (width-(width-40))
         new_height = (height-80)
         font = ImageFont.truetype("FONTS/arial.ttf",50)
-        draw.text((new_width,new_height),Date_Time,(0,0,0),font=font)
+        draw.text((new_width,new_height),Date_Time + ', '+ location,(0,0,0),font=font)
     else:
         new_width = (width-(width-20))
         new_height = (height-50)
         font = ImageFont.truetype("FONTS/arial.ttf",25)
-        draw.text((new_width,new_height),Date_Time,(0,0,0),font=font)    
+        draw.text((new_width,new_height),Date_Time + ', '+ location,(0,0,0),font=font)    
         
     data_add_image = image.save("COPY--"+image_name)
 
@@ -39,6 +40,7 @@ if __name__ == '__main__':
         exit(1)
     image = sys.argv[1]
     metadata = get_picture_metadata(image)
-    data_add_image = date_time_location_add_to_image(image, metadata)
+    location = get_location(image)
+    data_add_image = date_time_location_add_to_image(image, metadata,location)
     
 
