e90a09efba983b03af62e900c27f6784953c82a1
Baskaran Tharsan <Tharshanbasss@gmail.com>
2021-12-24 11:28:55 +0530

    Convert string to date object

--- meta_of_photo.py
+++ meta_of_photo.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from PIL.ExifTags import TAGS
+from datetime import datetime
 
 def meta(photo_name):
     photo = Image.open(photo_name)
@@ -8,7 +9,10 @@ def meta(photo_name):
         # get the tag name, instead of human unreadable tag id
         tag = TAGS.get(tag_id, tag_id)
         data = exifdata.get(tag_id)
-        # decode bytes 
+        if tag == 'DateTime':
+            data = data.replace(':','-',2)
+            data = datetime.fromisoformat(data)
+        # decode bytes
         if isinstance(data, bytes):
             data = data.decode()
         print(f"{tag:25}: {data}")
