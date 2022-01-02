081ff3e9e0f0a61aa573076ea0fd391b455514ff
Ravi Ravitharan <ravitharan@gmail.com>
2022-01-02 12:45:20 +0000

    Add Bootstrap album web page
    Bootstrap album link is https://getbootstrap.com/docs/4.0/examples/album/

--- webserver/album/views.py
+++ webserver/album/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from . import urls
+
+
+def index(request):
+    return render(request, 'album.html')
